f67e0206e47d5a4cbbcc73ac5b412c48d86905f0
Kir Neboula <newbie13xd@outlook.com>
2018-09-05 00:23:01 +0200

    Added offline support

--- script.js
+++ script.js
@@ -36,3 +36,14 @@ for (let i = 0; i < switchButtons.length; i++) {
             break;
     }
 }
+
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('sw.js', {
+        scope: './'
+    }).then(function(reg) {
+        console.log('[PWA Builder] Service worker has been registered for scope:'+ reg.scope);
+    });
+}
